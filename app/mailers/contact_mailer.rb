43a7a0a340e4c2a228f6b92823bac57f6f89857c
tomelsom <te28@hotmail.com>
2024-09-16 14:41:05 +0100

    improved styling and email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,9 @@
 class ContactMailer < ApplicationMailer
-  class ContactMailer < ApplicationMailer
-    default to: 'te28@hotmail.com' # Replace with your recipient email
+
+  default to: 'te28@hotmail.com' 
 
     def contact_email(contact)
       @contact = contact
       mail(from: @contact.email, subject: 'New Contact Form Submission')
     end
-  end
-
 end
