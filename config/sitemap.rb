5aa8acc41a26366eb74bce58a0e02913e765578b
tomelsom <te28@hotmail.com>
2025-10-17 23:14:08 +0100

    improved the sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,6 +3,9 @@ require 'rubygems'
 require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = "https://www.absolutehealthandphysio.co.uk"
+
 SitemapGenerator::Sitemap.create do
-  add '/', :changefreq => 'weekly', :priority => 1.0
+  add '/', changefreq: 'weekly', priority: 1.0
+  add '/pessary_fitting', changefreq: 'monthly', priority: 0.9
+  add '/contact', changefreq: 'monthly', priority: 0.8 if Rails.application.routes.url_helpers.respond_to?(:contact_path)
 end
