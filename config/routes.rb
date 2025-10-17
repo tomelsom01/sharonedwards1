5802b00e1fb5e150325a028f2287cec8702d3fc6
tomelsom <te28@hotmail.com>
2025-10-17 23:19:19 +0100

    Redirect pessary-fitting to pessary_fitting

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   post "/cookies/accept", to: "cookies#accept"
   post "/cookies/reject", to: "cookies#reject"
   get '/pessary_fitting', to: 'pages#about'
+  get '/pessary-fitting', to: redirect('/pessary_fitting', status: 301)
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
