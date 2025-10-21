abcbc7e94b89c7ef8f39099cce96e521754633aa
tomelsom <te28@hotmail.com>
2025-10-21 12:21:30 +0100

    updated tel

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,11 +1,13 @@
-# config/sitemap.rb
-require 'rubygems'
 require 'sitemap_generator'
 
-SitemapGenerator::Sitemap.default_host = "https://www.absolutehealthandphysio.co.uk"
+SitemapGenerator::Sitemap.default_host = "https://www.physiotherapyrichmond.co.uk"
+SitemapGenerator::Sitemap.public_path = 'public/'
+SitemapGenerator::Sitemap.create_index = true
 
 SitemapGenerator::Sitemap.create do
-  add '/', changefreq: 'weekly', priority: 1.0
-  add '/pessary_fitting', changefreq: 'monthly', priority: 0.9
-  add '/contact', changefreq: 'monthly', priority: 0.8 if Rails.application.routes.url_helpers.respond_to?(:contact_path)
+  helpers = Rails.application.routes.url_helpers
+
+  add helpers.root_path, changefreq: 'weekly', priority: 1.0
+  add helpers.pessary_fitting_path, changefreq: 'monthly', priority: 0.9 if helpers.respond_to?(:pessary_fitting_path)
+  add helpers.contact_path, changefreq: 'monthly', priority: 0.8 if helpers.respond_to?(:contact_path)
 end
